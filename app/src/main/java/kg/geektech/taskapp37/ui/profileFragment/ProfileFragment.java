2af86a5ee54cd640d292124315a95d7071b14856
AskarIbraimov <raksakgz@gmail.com>
2021-11-29 15:38:26 +0600

    Initial commit

---
+++ app/src/main/java/kg/geektech/taskapp37/ui/profileFragment/ProfileFragment.java
@@ -0,0 +1,53 @@
+package kg.geektech.taskapp37.ui.profileFragment;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kg.geektech.taskapp37.databinding.FragmentProfileBinding;
+
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        find();
+    }
+
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    binding.imageView.setImageURI(uri);
+                }
+            });
+    private void find() {
+
+        binding.imageView.setOnClickListener(view -> {
+            mGetContent.launch("image/*");
+        });
+    }
+
+}
